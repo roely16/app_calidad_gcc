8b7f6cb7fc2a6e7edb63c42208abbdf397894094
Roely Chur <gerson.roely@gmail.com>
2022-02-10 11:03:59 -0600

    Alerta cuando la sesión ha expirado

--- src/views/Control.vue
+++ src/views/Control.vue
@@ -6,6 +6,25 @@
             </v-card>
 
             <Tabla />
+
+            <!-- <v-row v-if="status == 102" class="mt-4" justify="center">
+                <v-col cols="6">
+                    <v-alert
+                        prominent
+                        type="error"
+                    >
+                        <v-row align="center">
+                            <v-col class="grow">
+                                {{ message }}
+                            </v-col>
+                            <v-col class="shrink">
+                                <v-btn>IR</v-btn>
+                            </v-col>
+                        </v-row>
+                    </v-alert>
+                </v-col>
+            </v-row> -->
+            
         </v-container>
     </div>
 </template>
@@ -28,7 +47,36 @@
                     },
                    
                 ],
+                status: null,
+                message: null
             }
+        },
+        methods: {
+
+            obtener_usuario(){
+
+                let data = {
+                    name: "obtener_usuario",
+                    param: {
+
+                    },
+                };
+
+                this.axios.post(process.env.VUE_APP_API_URL, data)
+                .then((response) => {
+                    // eslint-disable-next-line no-console
+                    console.log(response.data);
+                    this.status = response.data.response.status
+                    this.message = response.data.response.result
+                })
+
+            }
+
+        },
+        mounted(){
+
+            this.obtener_usuario()
+
         }
     }
 </script>
